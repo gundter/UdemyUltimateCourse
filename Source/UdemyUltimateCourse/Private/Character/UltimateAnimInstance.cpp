4d05c110645a3af3e7aebbbd0060044034ceb0cf
Terry Gunderson <tgunderson25@gmail.com>
2023-08-10 11:30:01 -0500

    Create C++ Anim Instance

---
+++ Source/UdemyUltimateCourse/Private/Character/UltimateAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Copyright Serico Games
+
+
+#include "Character/UltimateAnimInstance.h"
+
+#include "Character/UltimateCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UUltimateAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	CharacterRef = Cast<AUltimateCharacter>(TryGetPawnOwner());
+	if (CharacterRef)
+	{
+		CharacterMovement = CharacterRef->GetCharacterMovement();
+	}
+	
+}
+
+void UUltimateAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (CharacterMovement == nullptr) return;
+	
+	Velocity = CharacterMovement->Velocity;
+	GroundSpeed = UKismetMathLibrary::VSizeXY(Velocity);
+	bIsFalling = CharacterMovement->IsFalling();
+	bShouldMove = GroundSpeed > 3.f;
+}
---
+++ Source/UdemyUltimateCourse/Public/Character/UltimateAnimInstance.h
@@ -0,0 +1,40 @@
+// Copyright Serico Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "UltimateAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+class AUltimateCharacter;
+/**
+ * 
+ */
+UCLASS()
+class UDEMYULTIMATECOURSE_API UUltimateAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AUltimateCharacter> CharacterRef;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	TObjectPtr<UCharacterMovementComponent> CharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bShouldMove;
+};
