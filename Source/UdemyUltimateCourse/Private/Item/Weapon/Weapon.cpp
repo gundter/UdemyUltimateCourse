ee655d9e7e2f727d05e9917cfb4e85ed758d3bf9
Terry Gunderson <tgunderson25@gmail.com>
2023-08-10 18:06:21 -0500

    Create Weapon Class

---
+++ Source/UdemyUltimateCourse/Private/Item/Weapon/Weapon.cpp
@@ -0,0 +1,16 @@
+// Copyright Serico Games
+
+
+#include "Item/Weapon/Weapon.h"
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bSweep, SweepResult);
+}
+
+void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
---
+++ Source/UdemyUltimateCourse/Public/Item/Weapon/Weapon.h
@@ -0,0 +1,20 @@
+// Copyright Serico Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYULTIMATECOURSE_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bSweep, const FHitResult& SweepResult) override;
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+};
